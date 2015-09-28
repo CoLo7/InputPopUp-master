b8f36fb01c2e8530b2d9636e8ee323a742bf0592
CoLo7 <gabeaguilar7@gmail.com>
2015-09-28 09:33:18 -0600

    Added Comments to InputPopUp

---
+++ src/input/controller/InputRunner.java
@@ -0,0 +1,10 @@
+package input.controller;
+
+public class InputRunner
+{
+	public static void main(String [] args)
+	{
+		PopupController myController = new PopupController();
+		myController.start();
+	}
+}
---
+++ src/input/model/Thingy.java
@@ -0,0 +1,43 @@
+package input.model;
+
+/**
+ *Creates a Thingy object to show access and proper variable types 
+ * @author gagu5735
+ * 
+ */
+ 
+public class Thingy
+{
+	private String name;
+	private int age;
+	private double weight;
+	
+	public Thingy()
+	{
+		name ="";
+		age = -99;
+		weight = -.008;
+	}
+	
+	public Thingy(String name, int age, double weight)
+	{
+		//this. refers to the current instance of this object (A thingy)
+		this.name = name;
+		this.age = age;
+		this.weight = weight;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	public double getWeight()
+	{
+		return weight; 
+	}
+	
+}
